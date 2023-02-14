1bd85428194eff920a578fe82b711d9d5d297db1
eons <eons@eons.dev>
2023-02-13 18:55:16 -0800

    provide basic functionality

--- README.md
+++ README.md
@@ -3,23 +3,21 @@ This script allows you to build `deployment` projects.
 Essentially, this is an alternative to [Helm templates](https://helm.sh/docs/chart_best_practices/templates/). The goal is to use Python and the [Eons Library](https://github.com/eons-dev/eons.lib) to provide a more powerful and more intuitive templating system. This Builder also uses the [Eons Infrastructure Technologies](https://infrastructure.tech) package repository in place of a [Helm chart repo](https://helm.sh/docs/topics/chart_repository/).
 
 ## Output
-This Builder compiles all `*.yaml` files in the `src` folder, all dependencies in the `dep` folder, and all dependencies missing from `dep` that can be found online. It puts this compiled output into 2 separate .yaml files:
-1. Library: a compiled but uninterpreted file for use as a dependency in other deployments (i.e. no value substitution).
-2. Helm: a compiled and interpreted file which it then wraps in a Helm chart for use in k8s (i.e. with value substitution).
+This Builder compiles all `*.yaml` files in the `src` folder, all dependencies in the `dep` folder, and all dependencies missing from `dep` that can be found online. It puts this compiled output into a single .yaml file, usually `out/compiled.yaml`.
 
-It is recommended that you publish the first output, the "library" to the Infrastructure Technologies repo. This can be done with [publish.build](https://github.com/eons-dev/publish.build).
+It is recommended that you publish this output to the Infrastructure Technologies repo for use as a dependency in other deployments. This can be done with [publish.build](https://github.com/eons-dev/publish.build).
 
-While this Builder offers an alternative to Helm in many ways, in the end, it outputs a Helm chart. This is done both to make deployment within Kubernetes easier and to enable compatibility with existing Helm charts.
+To install the generated chart, use the [deploy.merx](https://github.com/eons-dev/deploy.merx) through [EMI](https://github.com/eons-dev/emi.exe).
 
-However, if you'd like to bypass Helm altogether, just `kubectl apply -f helm/templates/compiled.yaml`.
+## Naming
+Each source file will be placed in a yaml object of the file's base name (everything up to the first '.'. This acts similar to a namespace, and should be unnoticeable to you. However, should you need to specify configuration values later, you may do so by creating a yaml or json object of the file's name.  
+
+For example, if you have something like `service: {name: ...}` in both a `mysql.yaml` and a `redis.yaml` file, you can set the `mysql.service.name` to address the service name used in mysql.yaml.
+
+The same is true of dependencies: each dependency will be placed in it's own object based in the dependency name up to the first '.'.
 
 ## Options
-* `"description"` ("Helm chart of some kind") - chart description.
-* `"version"` ("0.0.0") - version for both version and appVersion.
-* `"library_output_folder"` ("lib") - where to put the compiled but unsubstituted yaml file.
-* `"helm_output_folder"` ("helm") - where to put the generated helm chart.
+* `"output_folder"` ("out") - where to put the generated file.
 * `"output_file"` ("compiled.yaml") - the name of the single yaml file to generate.
-* `"dependencies"` ([]) - an array of dependencies; for example `["my_dependency.deployment"]`.
-* `"api_version"` ("v2") - See [the Helm docs](https://helm.sh/docs/topics/charts/#the-apiversion-field).
-* `"chart_type"` ("application") - See [the Helm docs](https://helm.sh/docs/topics/charts/#chart-types).
-* `"chart_additional"` ("") - anything extra to add to `Chart.yaml`; note that if you're setting this in json, double escape your newlines (`\\n`).
+* `"dependencies"` ([]) - an array of dependencies; for example `["my_dependency.deployment"]`; versions are not allowed at this time.
+* `"dependencies_as` ({}) - a dictionary of dependencies and what they should be called (e.g. if you use the same dependency more than once).
--- src/deployment.py
+++ src/deployment.py
@@ -10,23 +10,20 @@ class deployment(Builder):
     def __init__(this, name="Kubernetes Deployment Builder"):
         super().__init__(name)
 
-        this.optionalKWArgs["description"] = "Helm chart of some kind"
-        this.optionalKWArgs["version"] = "0.0.0"
-        this.optionalKWArgs["library_output_folder"] = "lib"
-        this.optionalKWArgs["helm_output_folder"] = "helm"
+        this.optionalKWArgs["output_folder"] = "out"
         this.optionalKWArgs["output_file"] = "compiled.yaml"
         this.optionalKWArgs["dependencies"] = []
-        this.optionalKWArgs["api_version"] = "v2"
-        this.optionalKWArgs["chart_type"] = "application"
-        this.optionalKWArgs["chart_additional"] = ""
+        this.optionalKWArgs["dependencies_as"] = {}
 
         this.supportedProjectTypes = [
             "deployment"
         ]
 
+        this.originalRepoStore = None
+
     # Required Builder method. See that class for details.
     def DidBuildSucceed(this):
-        results = [this.library_output_folder, this.helm_output_folder]
+        results = [this.output_folder] # list in case we want to compile to multiple outputs.
         logging.debug(f"Checking if build was successful; output should be in {results}")
         return all([os.listdir(res) for res in results])
 
@@ -34,72 +31,6 @@ class deployment(Builder):
     def Build(this):
         this.MeetDependencies()
         this.Compile()
-        this.WriteMeta()
-
-    # Write all the files necessary to make this a helm chart.
-    def WriteMeta(this):
-        chart = this.CreateFile(Path(this.helm_output_folder).joinpath("Chart.yaml"))
-        chart.write(f'''\
-apiVersion: {this.api_version}
-name: {this.projectName}
-description: {this.description}
-
-# A chart can be either an 'application' or a 'library' chart.
-#
-# Application charts are a collection of templates that can be packaged into versioned archives
-# to be deployed.
-#
-# Library charts provide useful utilities or functions for the chart developer. They're included as
-# a dependency of application charts to inject those utilities and functions into the rendering
-# pipeline. Library charts do not define any templates and therefore cannot be deployed.
-type: {this.chart_type}
-
-# This is the chart version. This version number should be incremented each time you make changes
-# to the chart and its templates, including the app version.
-# Versions are expected to follow Semantic Versioning (https://semver.org/)
-version: {this.version}
-
-# This is the version number of the application being deployed. This version number should be
-# incremented each time you make changes to the application. Versions are not expected to
-# follow Semantic Versioning. They should reflect the version the application is using.
-appVersion: {this.version}
-
-{this.chart_additional}
-''')
-        chart.close()
-
-        ignore = this.CreateFile(Path(this.helm_output_folder).joinpath(".helmignore"))
-        ignore.write(f'''\
-# Patterns to ignore when building packages.
-# This supports shell glob matching, relative path matching, and
-# negation (prefixed with !). Only one pattern per line.
-.DS_Store
-# Common VCS dirs
-.git/
-.gitignore
-.bzr/
-.bzrignore
-.hg/
-.hgignore
-.svn/
-# Common backup files
-*.swp
-*.bak
-*.tmp
-*.orig
-*~
-# Various IDEs
-.project
-.idea/
-*.tmproj
-.vscode/
-''')
-        ignore.close()
-
-        values = this.CreateFile(Path(this.helm_output_folder).joinpath("values.yaml"))
-        # We don't use the values file.
-        values.close()
-
 
     # Download all packages the project depends on and extract them to this.depPath
     def MeetDependencies(this):
@@ -108,10 +39,21 @@ appVersion: {this.version}
             this.originalRepoStore = this.executor.repo.store
             this.executor.repo.store = this.depPath
         
+        for dep, dest in this.dependencies_as:
+            destPath = Path(this.depPath).joinpath(f"{dest}")
+            tmpPath = Path(this.depPath).joinpath(f"{dep}")
+            if (destPath.exists()):
+                continue
+            if (tmpPath.exists()):
+                raise Exception(f"Cannot download {dep} to {tmpPath}")
+            this.executor.DownloadPackage(dep, registerClasses = False, createSubDirectory = True)
+            tmpPath.rename(destPath)
+
         for dep in this.dependencies:
             depPath = Path(this.depPath).joinpath(f"{dep}")
-            if (not depPath.exists()):
-                this.executor.DownloadPackage(dep, registerClasses = False, createSubDirectory = True)
+            if (depPath.exists()):
+                continue
+            this.executor.DownloadPackage(dep, registerClasses = False, createSubDirectory = True)
         
         this.executor.repo.store = this.originalRepoStore
         this.originalRepoStore = None
@@ -119,26 +61,36 @@ appVersion: {this.version}
 
     # Ingest all the source yaml files and write them to one big, compiled yaml file.
     def Compile(this):
-        this.libFile = this.CreateFile(Path(this.library_output_folder).joinpath("templates").joinpath(this.output_file))
-        this.helmFile = this.CreateFile(Path(this.helm_output_folder).joinpath("templates").joinpath(this.output_file))
+        this.outFile = this.CreateFile(Path(this.output_folder).joinpath(this.output_file))
 
         locs = [f"{this.depPath}/{dep}" for dep in this.dependencies]
         locs.append(this.srcPath)
         for loc in locs:
-            for file in glob.glob(f"{loc}/*.yaml"):
+            if (loc != this.srcPath):
+                locContextKey = loc.split('/')[-1].split('.')[0]
+                this.outFile.write(f"{{this.executor.PushGlobalContextKey('{locContextKey}')}}\n")
+            
+            for file in glob(f"{loc}/*.yaml"):
+                
+                # File context is a little too specific.
+                # Configs can be referenced by deployment name for now.
+                #
+                # fileContextKey = file.split('/')[-1].split('.')[0]
+                # this.outFile.write(f"{{this.executor.PushGlobalContextKey('{fileContextKey}')}}\n")
+
                 logging.debug(f"Ingesting {file}")
                 iFile = open(Path(file), 'r')
                 for line in iFile:
-                        this.libFile.write(line)
                     try:
-                        this.helmFile.write(eval(f"f\"{line[:-1]}\""))
-                        this.helmFile.write("\n")
+                        this.outFile.write(line)
                     except Exception as e:
                         logging.error(str(e))
-
-                this.libFile.write('---\n')
-                this.helmFile.write('---\n')
                 iFile.close()
+
+                # this.outFile.write(f"{{this.executor.PopGlobalContextKey('{fileContextKey}')}}\n")
+                this.outFile.write('\n---\n')
+
+            if (loc != this.srcPath):
+                this.outFile.write(f"{{this.executor.PopGlobalContextKey('{locContextKey}')}}\n")
         
-        this.libFile.close()
-        this.helmFile.close()
+        this.outFile.close()
